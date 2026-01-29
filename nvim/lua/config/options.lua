25d282204d314d7022a464f23429566e4b059c79
freitaspinhe <gabrielpinheiro2003@gmail.com>
2026-01-29 15:26:02 -0300

    mudando nvim pro lazyvim

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Habilita corretor para Inglês e Português
+vim.opt.spelllang = { "en", "pt_br" }
+
+-- Numeração absoluta (começa no 1)
+vim.opt.number = true
+vim.opt.relativenumber = false
+
+-- Desativa o corretor ortográfico globalmente
+vim.opt.spell = false
