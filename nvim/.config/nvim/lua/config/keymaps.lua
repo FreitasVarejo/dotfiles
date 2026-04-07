cff6fa2374307c8c7c9dd5af6900499a733ae82d
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-04-07 03:44:50 -0300

    feat: add toggleterm plugin and keymaps
    - Add toggleterm.lua plugin configuration with lazy loading
    - Update keymaps with toggleterm toggle command

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Resize splits with keyboard
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,18 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  event = "VeryLazy",
+  keys = {
+    { "<leader>tt", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Terminal (vertical)" },
+    { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Terminal (horizontal)" },
+    { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal (float)" },
+  },
+  opts = {
+    open_mapping = [[<c-\>]],
+    direction = "vertical",
+    size = 80,
+    hide_numbers = true,
+    shade_filetypes = {},
+    autochdir = true,
+  },
+}
