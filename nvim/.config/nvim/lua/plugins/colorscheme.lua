a5f21758b4c6917fdcfe31d087730e2ad29e33c6
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-04-11 09:39:57 -0300

    Revert "feat: add machine-specific theme detection for nvim and tmux"
    This reverts commit 1da109b406e24ce5568ad291187c7bea61eb1b6f.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,54 +1,12 @@
--- Machine-specific theme detection
-local function get_catppuccin_flavour()
-  local hostname = vim.fn.hostname()
-
-  -- Map hostnames to Catppuccin flavours
-  local flavour_map = {
-    ["fedora-41"] = "mocha",       -- gray/cool, neutral
-    ["pi02"] = "macchiato",         -- soft/warm, easier on low-light
-    ["BTGNOTE10113"] = "frappe",    -- cooler/blue, professional
-  }
-
-  -- Return matching flavour, or default to mocha
-  return flavour_map[hostname] or "mocha"
-end
-
 return {
-  -- Catppuccin colorscheme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = get_catppuccin_flavour(),
-      transparent_background = false,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = true,
-        mini = {
-          enabled = true,
-        },
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-      },
-    },
-  },
+  -- add dracula
+  { "Mofiqul/dracula.nvim" },
 
-  -- Configure LazyVim to load catppuccin-nvim (new name after breaking change)
+  -- Configure LazyVim to load dracula
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-nvim",
+      colorscheme = "dracula",
     },
   },
 }
