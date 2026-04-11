c7797b4e258ac2448759b7d448452d500084c04f
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-04-11 09:35:24 -0300

    Add tmux pane resizing keybinds and fix toggleterm indentation

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -7,7 +7,7 @@ return {
     { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Terminal (horizontal)" },
     { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal (float)" },
   },
-  opts = {
+   opts = {
     open_mapping = [[<c-\>]],
     direction = "vertical",
     size = 80,
