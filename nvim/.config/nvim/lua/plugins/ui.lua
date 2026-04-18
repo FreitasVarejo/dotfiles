0f6c3d52db828c66e6d2f8f64f8b7b8ba15cd0d6
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-04-18 19:11:42 -0300

    Replace catppuccin with solarized-osaka and remove zen-mode/twilight

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,56 @@
+return {
+  -- Colorscheme
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+
+  -- File Explorer (Yazi)
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<leader>-",
+        function()
+          require("yazi").yazi()
+        end,
+        desc = "Open yazi at the current file",
+      },
+      {
+        "<leader>cw",
+        function()
+          require("yazi").yazi(nil, vim.fn.getcwd())
+        end,
+        desc = "Open yazi at the current working directory",
+      },
+      {
+        "<leader>y",
+        function()
+          require("yazi").yazi()
+        end,
+        desc = "Resume the last yazi session",
+      },
+    },
+    opts = {
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+  },
+
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown" },
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  },
+}
