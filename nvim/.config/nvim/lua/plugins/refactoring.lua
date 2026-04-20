e2c61ab871bf7a5d980bdddff5f426789950ac32
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-04-20 06:33:35 -0300

    fix(nvim): add telescope as a dependency for refactoring.nvim

--- nvim/.config/nvim/lua/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/plugins/refactoring.lua
@@ -4,18 +4,54 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
     },
     config = function()
       require("refactoring").setup()
-      -- Carrega a extensão do Telescope
+      -- Agora o Telescope está garantido de estar carregado aqui
       require("telescope").load_extension("refactoring")
     end,
     keys = {
-      { "<leader>re", function() require("refactoring").refactor("Extract Function") end, mode = "x", desc = "Extrair Função" },
-      { "<leader>rf", function() require("refactoring").refactor("Extract Function To File") end, mode = "x", desc = "Extrair Função para Ficheiro" },
-      { "<leader>rv", function() require("refactoring").refactor("Extract Variable") end, mode = "x", desc = "Extrair Variável" },
-      { "<leader>ri", function() require("refactoring").refactor("Inline Variable") end, mode = { "n", "x" }, desc = "Inline Variável" },
-      { "<leader>rr", function() require("telescope").extensions.refactoring.refactors() end, mode = { "n", "x" }, desc = "Menu de Refactoring" },
+      {
+        "<leader>re",
+        function()
+          require("refactoring").refactor("Extract Function")
+        end,
+        mode = "x",
+        desc = "Extrair Função",
+      },
+      {
+        "<leader>rf",
+        function()
+          require("refactoring").refactor("Extract Function To File")
+        end,
+        mode = "x",
+        desc = "Extrair Função para Ficheiro",
+      },
+      {
+        "<leader>rv",
+        function()
+          require("refactoring").refactor("Extract Variable")
+        end,
+        mode = "x",
+        desc = "Extrair Variável",
+      },
+      {
+        "<leader>ri",
+        function()
+          require("refactoring").refactor("Inline Variable")
+        end,
+        mode = { "n", "x" },
+        desc = "Inline Variável",
+      },
+      {
+        "<leader>rr",
+        function()
+          require("telescope").extensions.refactoring.refactors()
+        end,
+        mode = { "n", "x" },
+        desc = "Menu de Refactoring",
+      },
     },
-  }
+  },
 }
