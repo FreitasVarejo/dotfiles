5e23a8b03f8468e0e0c234af902ae00a522a3296
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-05-02 11:09:29 -0300

    refatora keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,17 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local status_ok, discipline = pcall(require, "config.discipline")
+local status_ok, discipline = pcall(require, "config.custom-mode.discipline")
 if status_ok then
   discipline.cowboy()
 end
 
 -- Disable neo-tree explorer keymaps (using yazi instead)
-vim.keymap.del("n", "<leader>e")
-vim.keymap.del("n", "<leader>E")
+-- TODO: desativar o lugin do neo-tree todo
+-- vim.keymap.del("n", "<leader>e")
+-- vim.keymap.del("n", "<leader>E")
 
 -- Pair Coding Mode Toggle
-local pairmode = require("config.pairmode")
+local pairmode = require("config.custom-mode.pairmode")
 vim.keymap.set("n", "<leader>ee", function()
   pairmode.toggle()
 end, { desc = "Toggle Pair Coding Mode" })
@@ -24,15 +25,7 @@ vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decreas
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Prettier format (using built-in vim.lsp.buf.format with prettier)
-vim.keymap.set(
-  { "n", "v" },
-  "<leader>fm",
-  function()
-    vim.lsp.buf.format({ async = true })
-  end,
-  { desc = "Format (Prettier)" }
-)
+vim.keymap.set({ "n", "v" }, "<leader>fm", function() end, { desc = "Format (Prettier)" })
 
 -- ESLint fix
 vim.keymap.set("n", "<leader>el", "<cmd>EslintFixAll<cr>", { desc = "ESLint Fix All" })
-
