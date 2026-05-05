583d003054cbd1dcd564ad824a70ee3a3e28a3b0
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-05-04 21:34:13 -0300

    feat(nvim/dotnet): implement roslyn LSP configuration with debug and test support
    - Replace OmniSharp with modern Roslyn LSP (nvim-lspconfig native)
    - Add netcoredbg debugger support via nvim-dap
    - Add neotest-vstest integration for running .NET tests
    - Include inlay hints and code lens configuration
    - Configure csharpier as formatter
    - Ensure C# parser for TreeSitter

---
+++ nvim/.config/nvim/lua/plugins/lang/dotnet.lua
@@ -0,0 +1,106 @@
+return {
+  -- TreeSitter support for C#
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "c_sharp" },
+    },
+  },
+
+  -- Mason packages for .NET development
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "roslyn",     -- Roslyn LSP (C# language server)
+        "csharpier",  -- C# code formatter
+        "netcoredbg", -- .NET debugger
+      },
+    },
+  },
+
+  -- LSP Configuration for Roslyn
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        roslyn_ls = {
+          settings = {
+            -- Inlay hints configuration
+            ["csharp|inlay_hints"] = {
+              csharp_enable_inlay_hints_for_implicit_object_creation = true,
+              csharp_enable_inlay_hints_for_implicit_variable_types = true,
+              csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+              csharp_enable_inlay_hints_for_types = true,
+              dotnet_enable_inlay_hints_for_indexer_parameters = true,
+              dotnet_enable_inlay_hints_for_literal_parameters = true,
+              dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+              dotnet_enable_inlay_hints_for_other_parameters = true,
+              dotnet_enable_inlay_hints_for_parameters = true,
+              dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+              dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+              dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+            },
+            -- Code lens configuration
+            ["csharp|code_lens"] = {
+              dotnet_enable_references_code_lens = true,
+              dotnet_enable_tests_code_lens = true,
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- Debug Adapter Protocol (DAP) for .NET with netcoredbg
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+
+      -- Configure netcoredbg adapter
+      if not dap.adapters["netcoredbg"] then
+        dap.adapters["netcoredbg"] = {
+          type = "executable",
+          command = vim.fn.exepath("netcoredbg"),
+          args = { "--interpreter=vscode" },
+          options = {
+            detached = false,
+          },
+        }
+      end
+
+      -- Configure debug configurations for C#
+      if not dap.configurations["cs"] then
+        dap.configurations["cs"] = {
+          {
+            type = "netcoredbg",
+            name = "Launch file",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
+  },
+
+  -- Testing support with neotest and neotest-vstest
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "Issafalcon/neotest-vstest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-vstest"] = {
+          -- Configuration for neotest-vstest
+        },
+      },
+    },
+  },
+}
