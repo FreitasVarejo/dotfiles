176e8e32238ad8499dfd68946be3fbee1360ae51
Gabriel Freitas Pinheiro <gabrielpinheiro2003@gmail.com>
2026-05-11 08:10:40 -0300

    refactor(mason): consolidate package manager configuration
    - Centralize all ensure_installed packages in lua/plugins/mason.lua
    - Add UI improvements (rounded border, better icons)
    - Add keymap for quick access: <leader>cm opens Mason
    - Includes TypeScript, C#/.NET, Linting, and formatting tools
    - Removes duplicate Mason configuration from lang/dotnet.lua
    - Improves maintainability by having single source of truth for packages

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,36 @@
 return {
   "mason-org/mason.nvim",
+  cmd = "Mason",
+  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
   opts = {
+    ui = {
+      border = "rounded",
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗",
+      },
+    },
     registries = {
-      "github:Crashdummyy/mason-registry",
-      "github:mason-org/mason-registry",
+      "github:Crashdummyy/mason-registry", -- Custom registry (includes Roslyn)
+      "github:mason-org/mason-registry",   -- Official Mason registry
+    },
+    ensure_installed = {
+      -- TypeScript / JavaScript
+      "vtsls",           -- TypeScript language server
+      "prettier",        -- Code formatter
+
+      -- C# / .NET
+      "roslyn",          -- Roslyn C# language server
+      "csharpier",       -- C# code formatter
+      "netcoredbg",      -- .NET debugger
+
+      -- Linting
+      "eslint-lsp",      -- ESLint language server
+      "eslint_d",        -- Fast eslint runner
+
+      -- Other
+      "stylua",          -- Lua formatter
     },
   },
 }
